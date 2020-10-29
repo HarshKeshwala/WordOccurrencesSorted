56e8bcbfa58087e777a68b95d05bd8337066e79e
harshkeshwala <keshwalaharsh@gmail.com>
2020-10-29 19:32:16 -0400

    [Added method to sort words based on occurrences]

--- src/FileParser/SortedWordOccurrences.java
+++ src/FileParser/SortedWordOccurrences.java
@@ -4,7 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -48,7 +53,42 @@ public class SortedWordOccurrences {
 	    for (Map.Entry<String, Integer> en : wordsMap.entrySet()) { 
             System.out.println("Key = " + en.getKey() +  
                           ", Value = " + en.getValue()); 
-        } 
+        }
 	    
+	    System.out.println("************************* SORTED LIST *******************************");
+		  
+	    //Storing results in to HashMap after being sorted out
+	    HashMap<String, Integer> sortedMap = sortWordsByOccurrence(wordsMap);
+	    
+	    for (Map.Entry<String, Integer> en : sortedMap.entrySet()) { 
+            System.out.println("Word = " + en.getKey() +  
+                          ", Count = " + en.getValue()); 
+        }
 	}
+	
+	// Method to sort words based on occurrence
+	public static HashMap<String, Integer> sortWordsByOccurrence(HashMap<String, Integer> map) 
+    { 
+        // Transfer data to List from HashMap to sort
+        List<Map.Entry<String, Integer> > list = 
+               new LinkedList<Map.Entry<String, Integer> >(map.entrySet()); 
+  
+        // Sorting the list 
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >()
+        { 
+            public int compare(Map.Entry<String, Integer> o1,  
+                               Map.Entry<String, Integer> o2) 
+            { 
+                return (o2.getValue()).compareTo(o1.getValue()); 
+            } 
+        }); 
+          
+        // Transfer sorted data back to the HashMap again
+        HashMap<String, Integer> resultedMap = new LinkedHashMap<String, Integer>(); 
+        for (Map.Entry<String, Integer> obj : list)
+        { 
+        	resultedMap.put(obj.getKey(), obj.getValue()); 
+        } 
+        return resultedMap; 
+    } 
 }
