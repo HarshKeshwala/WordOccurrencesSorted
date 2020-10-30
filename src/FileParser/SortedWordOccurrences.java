4a9a17e8cd73f1fb37707ae9405283ccd5e3f9aa
harshkeshwala <keshwalaharsh@gmail.com>
2020-10-29 20:36:07 -0400

    [Modularised functionalities of Getting words, Sorting, and Generating report]

--- src/FileParser/SortedWordOccurrences.java
+++ src/FileParser/SortedWordOccurrences.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -19,79 +21,60 @@ import java.util.Scanner;
 public class SortedWordOccurrences {
 
 	
-	public static void main(String[] args)  throws IOException 
+	public static void main(String[] args)  throws Exception 
 	{
-		//Parsing data from the file
-		File file = new File("/Users/harshkeshwala/Documents/JavaWorkspace/HarshSanjayCodeTest/src/paragraph.txt"); 
-        FileInputStream fileInputStream = new FileInputStream(file); 
-       
-        //Path of HTML file to print result in
-        File file2 = new File("/Users/harshkeshwala/Documents/JavaWorkspace/HarshSanjayCodeTest/src/result.html");
-
-        Scanner inputFile = new Scanner(fileInputStream);
-
-        //Arraylist to store the words parsed from the paragraph
+		//Path of the input file
+		Path pathInputFile = Paths.get("src/paragraph.txt");
+  
+		//Path of HTML file to print the result to
+		Path pathOutputFile = Paths.get("src/report.html");
+        
+        //HashMap to store words from the file
+        HashMap<String, Integer> wordsMap = getWordsFromFile(pathInputFile);
+	  
+	    //Storing results in to HashMap after words being sorted out based on occurrence
+	    HashMap<String, Integer> sortedMap = sortWordsByOccurrence(wordsMap);
+	    
+	    //Calling method to write sorted words report to HTML file
+	    printReportToHTML(sortedMap, pathOutputFile);
+	    
+	}
+	
+	//Method to get words from the input file
+	public static HashMap<String, Integer> getWordsFromFile(Path pathInputFile) throws IOException
+	{
+		
+		FileInputStream fileInputStream = new FileInputStream(pathInputFile.toFile()); 
+	       
+		Scanner inputFile = new Scanner(fileInputStream);
+		
+		//Arraylist to store the words parsed from the paragraph
         ArrayList<String> data = new ArrayList<String>();
 
         //Adding words to the Arraylist from paragraph file
-        while(inputFile.hasNext()) {   	
+        while(inputFile.hasNext()) {
         	data.add(inputFile.next());
         }
+       
         inputFile.close();
         fileInputStream.close();
 
-        
-        //Initialising HashMap
         HashMap<String, Integer> wordsMap = new HashMap<String, Integer>();
-
+        
         //Storing words in to HashMap based on Occurrences from ArrayList
-	    for (String s : data) {
+	    for (String s : data)
+	    {
 	    	s = s.toLowerCase();	//convert each word to lower case
-	        if (wordsMap.containsKey(s)) {
+	        if (wordsMap.containsKey(s))
+	        {
 	        	wordsMap.put(s, wordsMap.get(s) + 1);
-	        } else {
+	        } else
+	        {
 	        	wordsMap.put(s, 1);
 	        }
 	    }
-	    
-	    //Printing HashMap entries to the console
-	    for (Map.Entry<String, Integer> en : wordsMap.entrySet()) { 
-            System.out.println("Key = " + en.getKey() +  
-                          ", Value = " + en.getValue()); 
-        }
-	    
-	    System.out.println("************************* SORTED LIST *******************************");
-		  
-	    //Storing results in to HashMap after being sorted out
-	    HashMap<String, Integer> sortedMap = sortWordsByOccurrence(wordsMap);
-	    
-	    for (Map.Entry<String, Integer> en : sortedMap.entrySet()) { 
-            System.out.println("Word = " + en.getKey() +  
-                          ", Count = " + en.getValue()); 
-        }
-	    
-	    //Writing data in to HTML files
-	    try 
-	    {	
-       	 BufferedWriter bw = new BufferedWriter(new FileWriter(file2));
-       	 bw.write("<div><center><h1>Word count occurrence</h1>");
-       	 bw.write("<table border='1'>");
-       	 bw.write("<tr><td>Words</td><td>Occurrence</td></tr>");
-       
-       	 for(Map.Entry<String, Integer> en : sortedMap.entrySet())
-       	 {
-            bw.write("<tr><td>"+ en.getKey()+ "</td><td>" +en.getValue()+"</tr>");
-         }
-       	 
-       	 bw.write("</table>");
-       	 bw.write("</center></div>");
-       	 bw.close();
-       	 
-	    }
-	    catch(IOException e)
-	    {
-	    	e.printStackTrace();
-	    }
+		
+		return wordsMap;
 	}
 	
 	// Method to sort words based on occurrence
@@ -118,5 +101,33 @@ public class SortedWordOccurrences {
         	resultedMap.put(obj.getKey(), obj.getValue()); 
         } 
         return resultedMap; 
-    } 
+    }
+	
+	// Methods to write result into the HTML file
+	public static void printReportToHTML(HashMap<String, Integer> sortedMap, Path pathOutputFile) throws Exception
+	{
+	    //Writing data in to HTML files
+	    try 
+	    {	
+       	 BufferedWriter bw = new BufferedWriter(new FileWriter(pathOutputFile.toFile()));
+       	 bw.write("<div><center><h1>Word count occurrence</h1>");
+       	 bw.write("<table border='1'>");
+       	 bw.write("<tr><td>Words</td><td>Occurrence</td></tr>");
+       
+       	 for(Map.Entry<String, Integer> en : sortedMap.entrySet())
+       	 {
+            bw.write("<tr><td>"+ en.getKey()+ "</td><td>" +en.getValue()+"</tr>");
+         }
+       	 
+       	 bw.write("</table>");
+       	 bw.write("</center></div>");
+       	 bw.close();
+       	 
+	    }
+	    catch(IOException e)
+	    {
+	    	e.printStackTrace();
+	    }
+
+	}
 }
